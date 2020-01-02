60ae87a56d60cb91fd2b994211160da1e034d8d9
BARIS KANTAR <baris.kantar@alnoortv.com>
2020-01-02 17:05:10 +0300

    initialize commit

---
+++ src/test/java/com/bisaha/cbd/CbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bisaha.cbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
